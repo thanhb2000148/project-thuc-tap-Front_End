38ca3123cdc3805d87a605869eab79e670b85632
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-11 11:39:56 +0700

    category

---
+++ src/services/category.service.js
@@ -0,0 +1,16 @@
+import createApiClient from "./api.service";
+class CategoryService {
+  constructor(baseUrl = "/api/v1/category") {
+    this.api = createApiClient(baseUrl);
+  }
+  async getAll() {
+    try {
+      const response = await this.api.get("/");
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
+export default new CategoryService();
