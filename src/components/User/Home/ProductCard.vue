b677a0923c88e4deba63b0aaf4b614dbc9a4fb38
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-12 00:46:43 +0700

    add productCard update category.service update homeView

---
+++ src/components/User/Home/ProductCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="rounded position-relative fruite-item">
+    <div class="fruite-img">
+      <img :src="productImage" class="img-fluid w-100 rounded-top" alt="" />
+    </div>
+    <!-- <div
+      class="text-white bg-secondary px-3 py-1 rounded position-absolute"
+      style="top: 10px; left: 10px"
+    >
+      {{ product.CATEGORY_ID }}
+    </div> -->
+    <div class="p-4 border border-secondary border-top-0 rounded-bottom">
+      <h4>{{ product.NAME_PRODUCT }}</h4>
+      <p>{{ product.SHORT_DESC }}</p>
+      <div class="d-flex justify-content-between flex-lg-wrap">
+        <p class="text-dark fs-5 fw-bold mb-0">{{ productPrice }} / kg</p>
+        <a
+          href="#"
+          class="btn border border-secondary rounded-pill px-3 text-primary"
+        >
+          <i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    product: Object,
+  },
+  computed: {
+    productImage() {
+      return this.product.LIST_FILE_ATTACHMENT_DEFAULT[0].FILE_URL; // Cập nhật tùy thuộc vào cấu trúc thực tế
+    },
+    productPrice() {
+      return this.product.PRICE; // Cập nhật tùy thuộc vào cấu trúc thực tế
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add any specific styles for ProductCard if needed */
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,11 +12,11 @@
             </div>
             <div class="col-lg-8 text-end">
               <ul class="nav nav-pills d-inline-flex text-center mb-5">
-                <li class="nav-item">
+                <li class="nav-item cursor-pointer">
                   <a
-                    class="d-flex m-2 py-2 bg-light rounded-pill active"
-                    data-bs-toggle="pill"
-                    href="#tab-1"
+                    class="d-flex m-2 py-2 bg-light rounded-pill"
+                    :class="{ active: currentTab === 'all' }"
+                    @click="setCurrentTab('all')"
                   >
                     <span class="text-dark" style="width: 130px"
                       >Tất cả sản phẩm</span
@@ -24,14 +24,14 @@
                   </a>
                 </li>
                 <li
-                  class="nav-item"
+                  class="nav-item cursor-pointer"
                   v-for="(item, index) in nameCategory"
                   :key="index"
                 >
                   <a
                     class="d-flex py-2 m-2 bg-light rounded-pill"
-                    data-bs-toggle="pill"
-                    :href="`#tab-${index}`"
+                    :class="{ active: currentTab === item.CATEGORY_NAME }"
+                    @click="setCurrentTab(item.CATEGORY_NAME)"
                   >
                     <span class="text-dark" style="width: 130px">{{
                       item.CATEGORY_NAME
@@ -43,98 +43,33 @@
           </div>
           <div class="tab-content">
             <div
-              id="tab-1"
+              v-if="currentTab === 'all'"
               class="tab-pane fade show p-0 active"
-              v-for="item in productCategory"
-              :key="item._id"
             >
               <div class="row g-4">
-                <div class="col-lg-12">
-                  <div class="row g-4">
-                    <div class="col-md-6 col-lg-4 col-xl-3">
-                      <div class="rounded position-relative fruite-item">
-                        <div class="fruite-img">
-                          <img
-                            src="img/fruite-item-5.jpg"
-                            class="img-fluid w-100 rounded-top"
-                            alt=""
-                          />
-                        </div>
-                        <div
-                          class="text-white bg-secondary px-3 py-1 rounded position-absolute"
-                          style="top: 10px; left: 10px"
-                        >
-                          Fruits
-                        </div>
-                        <div
-                          class="p-4 border border-secondary border-top-0 rounded-bottom"
-                        >
-                          <h4>Grapes</h4>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit sed do eiusmod te incididunt
-                          </p>
-                          <div
-                            class="d-flex justify-content-between flex-lg-wrap"
-                          >
-                            <p class="text-dark fs-5 fw-bold mb-0">
-                              $4.99 / kg
-                            </p>
-                            <a
-                              href="#"
-                              class="btn border border-secondary rounded-pill px-3 text-primary"
-                              ><i
-                                class="fa fa-shopping-bag me-2 text-primary"
-                              ></i>
-                              Add to cart</a
-                            >
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div class="col-md-6 col-lg-4 col-xl-3">
-                      <div class="rounded position-relative fruite-item">
-                        <div class="fruite-img">
-                          <img
-                            src="img/fruite-item-5.jpg"
-                            class="img-fluid w-100 rounded-top"
-                            alt=""
-                          />
-                        </div>
-                        <div
-                          class="text-white bg-secondary px-3 py-1 rounded position-absolute"
-                          style="top: 10px; left: 10px"
-                        >
-                          Fruits
-                        </div>
-                        <div
-                          class="p-4 border border-secondary border-top-0 rounded-bottom"
-                        >
-                          <h4>Grapes</h4>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit sed do eiusmod te incididunt
-                          </p>
-                          <div
-                            class="d-flex justify-content-between flex-lg-wrap"
-                          >
-                            <p class="text-dark fs-5 fw-bold mb-0">
-                              $4.99 / kg
-                            </p>
-                            <a
-                              href="#"
-                              class="btn border border-secondary rounded-pill px-3 text-primary"
-                              ><i
-                                class="fa fa-shopping-bag me-2 text-primary"
-                              ></i>
-                              Add to cart</a
-                            >
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                <div
+                  v-for="product in allProducts"
+                  :key="product._id"
+                  class="col-md-6 col-lg-4 col-xl-3"
+                >
+                  <ProductCard :product="product" :category="category" />
+                </div>
+              </div>
+            </div>
+            <div
+              v-else
+              v-for="(category, index) in nameCategory"
+              :key="category._id"
+              class="tab-pane fade show p-0"
+              :class="{ active: currentTab === category.CATEGORY_NAME }"
+            >
+              <div class="row g-4">
+                <div
+                  v-for="product in productCategory[index]"
+                  :key="product._id"
+                  class="col-md-6 col-lg-4 col-xl-3"
+                >
+                  <ProductCard :product="product" />
                 </div>
               </div>
             </div>
@@ -147,13 +82,14 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import NavBar from "@/components/User/layout/NavBar.vue";
 import AppFooter from "@/components/User/layout/AppFooter.vue";
 import Featurs from "@/components/User/Home/Featurs.vue";
 import Hero from "@/components/User/Home/Hero.vue";
+import ProductCard from "@/components/User/Home/ProductCard.vue";
 import categoryService from "@/services/category.service";
 import productService from "@/services/product.service";
+
 export default {
   name: "HomeView",
   components: {
@@ -161,12 +97,19 @@ export default {
     AppFooter,
     Featurs,
     Hero,
+    ProductCard,
   },
   data() {
     return {
       nameCategory: [],
       productCategory: [],
+      currentTab: "all",
     };
+  },
+  computed: {
+    allProducts() {
+      return this.productCategory.flat();
+    },
   },
   async created() {
     try {
@@ -179,6 +122,9 @@ export default {
     }
   },
   methods: {
+    setCurrentTab(tab) {
+      this.currentTab = tab;
+    },
     async getCategory() {
       try {
         const response = await categoryService.getAll();
@@ -191,18 +137,13 @@ export default {
     },
     async getProductCategory() {
       try {
-        if (Array.isArray(this.nameCategory)) {
-          for (const category of this.nameCategory) {
-            const response = await productService.getProductByIdCategory(
-              category._id
-            );
-            if (response && response.data) {
-              this.productCategory.push(response.data);
-            }
-          }
-        } else {
-          console.error("category is not an array:", this.products);
-        }
+        const productPromises = this.nameCategory.map(async (category) => {
+          const response = await productService.getProductByIdCategory(
+            category._id
+          );
+          return response && response.data ? response.data : [];
+        });
+        this.productCategory = await Promise.all(productPromises);
       } catch (error) {
         console.log("error", error);
       }
