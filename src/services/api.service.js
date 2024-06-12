aa1b9506f3b553e678ec76bb9fb7d587d57fd0a4
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-12 11:33:45 +0700

    cart

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import getCookieValue from "@/utils/getCookie";
 const commonConfig = {
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
+    token: getCookieValue("access_token"),
   },
 };
+console.log(getCookieValue("access_token"));
 export default (baseURL) => {
   return axios.create({
     baseURL,
